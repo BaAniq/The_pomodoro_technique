45a1a1cf81997a8d6124ebd4b0271e31da206cb3
BaAniq <basia.anisimowicz@gmail.com>
2023-11-17 16:12:34 +0100

    Adding an image as a bacground, changing the color of bg

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from tkinter import *
 
+FONT_NAME = "Courier"
+YELLOW = "#f7f5dd"
+
 window = Tk()
+window.config(padx=100, pady=50, bg=YELLOW)
 window.title('The Pomodoro Technique')
+
+image_background = Canvas(width=200, height=230, bg=YELLOW, highlightthickness=0)
+image_tomato = PhotoImage(file='tomato.png')
+image_background.create_image(100, 115, image=image_tomato)
+image_background.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+image_background.pack()
+
+
+
+
+
 
 
 
